dba30c8e9a94598d43acedb331c32ee3373272d8
Marco Camurri <mcamurri@robots.ox.ac.uk>
2021-05-05 13:20:56 +0100

    update dependencies

--- pronto_anymal_b/scripts/clone_deps.bash
+++ pronto_anymal_b/scripts/clone_deps.bash
@@ -3,7 +3,7 @@
 DEST=${CMAKE_PREFIX_PATH%%:*}/../src
 
 # Pronto core libraries
-git clone -b pronto-anymal-example https://github.com/ori-drs/pronto.git ${DEST}/pronto
+git clone https://github.com/ori-drs/pronto.git ${DEST}/pronto
 
 # Pronto dependencies
 git clone https://github.com/ori-drs/common_utils.git ${DEST}/pronto_common_utils
@@ -19,9 +19,6 @@ git clone -b pronto-fovis https://github.com/ori-drs/fovis_ros.git ${DEST}/fovis
 # Tools to visualize path in RViz
 git clone https://github.com/ori-drs/hector_slam.git ${DEST}/hector_slam
 
-# ANYmal B Simple Description package
-git clone -b add-sensors https://github.com/ori-drs/anymal_b_simple_description.git ${DEST}/anymal_b_simple_description
-
 # RealSense Description
-git clone -b development-fixes https://github.com/ori-drs/realsense.git ${DEST}/realsense
+git clone https://github.com/IntelRealSense/realsense-ros ${DEST}/realsense
 
